051a3deb304523e47dc50050abe32ea62a75bcd3
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2024-10-13 14:56:20 +0200

    Add files via upload

---
+++ end_inst.sh
@@ -0,0 +1,11 @@
+# this is a part of the installation script
+# will be automatically include in the overall installation script
+
+inst_certbot
+inst_f2b_jails
+inst_locate
+cd /root
+wget https://raw.githubusercontent.com/fdmgit/asterisk/main/inst_logo
+closing_msg
+
+#reboot
---
+++ fpbx17_installation.sh
@@ -0,0 +1,27 @@
+ #!/bin/bash
+
+cd /root
+wget https://raw.githubusercontent.com/fdmgit/asterisk/main/start_inst.sh
+wget https://raw.githubusercontent.com/FreePBX/sng_freepbx_debian_install/master/sng_freepbx_debian_install.sh -O fpbx_deb_inst.sh
+tail -n +2 fpbx_deb_inst.sh > fpbx.tmp && cp fpbx.tmp fpbx_deb_inst.sh
+wget https://raw.githubusercontent.com/fdmgit/asterisk/main/end_inst.sh
+
+touch full_fpbx17_inst.sh
+cat start_inst.sh fpbx_deb_inst.sh end_inst.sh > full_fpbx17_inst.sh
+
+chmod +x full_fpbx17_inst.sh
+
+rm fpbx.tmp
+rm start_inst.sh
+rm fpbx_deb_inst.sh
+rm end_inst.sh
+
+./full_fpbx17_inst.sh --skipversion --opensourceonly
+
+cd /root
+
+rm full_fpbx17_inst.sh
+
+reboot
+
+
