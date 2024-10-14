558efd99162853240603eff8921bd92bf4efab40
Flavio Del Mestre <29498370+fdmgit@users.noreply.github.com>
2024-10-14 16:12:00 +0200

    Update end_inst.sh

--- end_inst.sh
+++ end_inst.sh
@@ -2,7 +2,7 @@
 # will be automatically include in the overall installation script
 
 inst_certbot
-inst_f2b_jails
+#inst_f2b_jails
 inst_locate
 cd /root
 #wget https://raw.githubusercontent.com/fdmgit/customPBX/refs/heads/main/inst_logo
